570f53e9c38f13ba698c93b631bb6c4f2a01610e
burqa khatun <67106178+borkhakhatun@users.noreply.github.com>
2024-01-20 13:46:46 +0000

    old burqa,hijab,niqub.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '08b2b2dc380bb80b4654d09b8a947bf630ab82c5',
+    'name' => 'opauth/google',
+  ),
+  'versions' => 
+  array (
+    'opauth/google' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '08b2b2dc380bb80b4654d09b8a947bf630ab82c5',
+    ),
+    'opauth/opauth' => 
+    array (
+      'pretty_version' => '0.4.5',
+      'version' => '0.4.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3f979012d0bdf2d447bb02b97f7f7d9f482c77e8',
+    ),
+  ),
+);
